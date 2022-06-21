d9e8c1a34de3cbf7ee38d94a52cf0f435fa7a16f
Юлия Кудрова <bahtina_yul@mail.ru>
2022-06-21 21:26:15 +0800

    Added task arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,21 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
+  let result = arr1.every((n, i) => n === arr2[i] && arr1.length === arr2.length)
+    
   return result; // boolean
 }
 
+//console.log(compareArrays([8, 9], [6])) // false, разные значения
+//console. log(compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5])) // false, разные значения
+//console.log(compareArrays([9, 2, 4, 8, 2], [9, 2, 4])) // false, разные значения
+//console.log(compareArrays([1, 2, 3], [2, 3, 1])) // false, разные индексы, хотя и одинаковые значения
+//console.log(compareArrays([8, 1, 2], [8, 1, 2])) // true
+
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter((arr) => arr % 3 === 0 && arr > 0).map((arr) => arr * 10);
+  
   return resultArr; // array
 }
+
+//console.log(advancedFilter([-10, 12, -12, -3]))
+//console.log(advancedFilter([-6, 6, -21, 21]))
+//console.log(advancedFilter([0, 11, -0, 12]))
